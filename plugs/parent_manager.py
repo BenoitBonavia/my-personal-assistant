51989b26fabc88880b9376d3b034513c1e86d3dc
Benoit <benoit@wegrow-app.com>
2025-06-10 17:42:29 +0200

    ♻️ Refactoring the code

--- plugs/parent_manager.py
+++ plugs/parent_manager.py
@@ -1,16 +1,17 @@
 import logging
 
+logger = logging.getLogger(__name__)
+
 class ParentManager:
     def __init__(self):
-        self.logger = logging.getLogger(__name__)
         self.manager_name = "Parent"
 
     def handle_command(self, command):
         command_name = command['command_name']
         params = command.get('params', [])
-        self.logger.info("Executing command: %s with params: %s", command_name, params)
+        logger.info("Executing command: %s with params: %s", command_name, params)
         try:
             method_to_call = getattr(self, command_name)
             method_to_call(*params)
         except AttributeError:
-            self.logger.error("%s manager has no method named %s", self.manager_name, command_name)
+            logger.error("%s manager has no method named %s", self.manager_name, command_name)

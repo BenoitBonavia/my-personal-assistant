e419832e5ca18f2bf0f4f4c9bed112497caf29ec
Benoit <benoit@wegrow-app.com>
2025-07-22 19:05:43 +0200

    📈 Add sentry to have the logs

--- services/sentry_service.py
+++ services/sentry_service.py
@@ -1,9 +1,16 @@
 import sentry_sdk
+import logging
+from sentry_sdk.integrations.logging import LoggingIntegration
 
 class SentryService:
 
     def __init__(self):
+        sentry_logging = LoggingIntegration(
+            level=logging.INFO,
+            event_level=logging.ERROR
+        )
         sentry_sdk.init(
             dsn="https://3dced3899dece3439a4debe9149150a2@o4509713235378176.ingest.de.sentry.io/4509713236951120",
             send_default_pii=True,
+            integrations=[sentry_logging]
         )
